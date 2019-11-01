9b7151c550d5cf14e38692ccadab9ffe609e0a23
Charles Ellis <hamled@hamled.name>
2019-11-01 10:00:45 -0700

    tests pass

--- test/sportsball.test.cpp
+++ test/sportsball.test.cpp
@@ -1,6 +1,8 @@
 #include <mettle.hpp>
 using namespace mettle;
 
+#include "sportsball.hpp"
+
 suite<> sportsball("sportsball", [](auto &_) {
   _.test("is a class", []() {
     expect(std::is_class<Sportsball>::value, equal_to(true));
