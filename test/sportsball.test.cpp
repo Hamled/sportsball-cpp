60aa0b0d67dac52f5ae7daaa80cd1d683f42aa75
Charles Ellis <hamled@hamled.name>
2019-11-01 10:12:29 -0700

    getScore scores 0 points for no runners

--- test/sportsball.test.cpp
+++ test/sportsball.test.cpp
@@ -1,6 +1,7 @@
 #include <mettle.hpp>
 using namespace mettle;
 
+#include <string>
 #include "sportsball.hpp"
 
 suite<> sportsball("sportsball", [](auto &_) {
@@ -11,5 +12,12 @@ suite<> sportsball("sportsball", [](auto &_) {
     expect(
         std::is_member_function_pointer<decltype(&Sportsball::getScore)>::value,
         equal_to(true));
+  });
+  _.subsuite("getScore", [](auto &_) {
+    _.test("scores 0 points for no runners", []() {
+      Sportsball sb{};
+      auto result = sb.getScore();
+      expect(result, equal_to(std::string{"Home: 0 Away: 0"}));
+    });
   });
  });
