f107aeb4c427aff1f561886cafc19549a3e76bf7
Charles Ellis <hamled@hamled.name>
2019-11-01 10:39:55 -0700

    tests pass

--- src/sportsball.cpp
+++ src/sportsball.cpp
@@ -3,11 +3,39 @@
 #include <boost/format.hpp>
 #include "sportsball.hpp"
 
+Sportsball::Sportsball() {
+  this->away = 0;
+  this->home = 0;
+
+  this->resetFrame();
+}
+
 std::string Sportsball::getScore() {
-  return (boost::format("Home: 0 Away: %1%") % (this->runners - this->unscored)).str();
+  const int cur_away = this->is_away ? this->away + this->score() : this->away;
+  const int cur_home = this->is_away ? this->home : this->home + this->score();
+  return (boost::format("Home: %1% Away: %2%") % cur_home % cur_away).str();
 }
 
 void Sportsball::addEntry(int entry) {
+  if(!entry) {
+    if(++outs > 2) {
+      this->is_away
+        ? this->away += this->score()
+        : this->home += this->score();
+      this->resetFrame();
+    }
+    return;
+  }
+
   ++this->runners;
   this->unscored = std::min(BASES - entry, this->unscored) + 1;
+}
+
+int Sportsball::score() {
+  return this->runners - this->unscored;
+}
+
+void Sportsball::resetFrame() {
+  this->runners = this->unscored = this->outs = 0;
+  this->is_away = !this->is_away;
 }
--- src/sportsball.hpp
+++ src/sportsball.hpp
@@ -2,11 +2,20 @@
 
 class Sportsball {
 public:
+  Sportsball();
   std::string getScore();
   void addEntry(int entry);
 
 private:
+  int score();
+  void resetFrame();
+
   const static auto BASES = 3;
   int runners;
   int unscored;
+
+  int outs;
+  bool is_away;
+  int away;
+  int home;
 };
