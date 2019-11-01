744f6fbec2a3f89f860b5c818e1ab0490edd92a5
Charles Ellis <hamled@hamled.name>
2019-11-01 10:01:58 -0700

    Sportsball has a getScore method

--- test/sportsball.test.cpp
+++ test/sportsball.test.cpp
@@ -7,4 +7,9 @@ suite<> sportsball("sportsball", [](auto &_) {
   _.test("is a class", []() {
     expect(std::is_class<Sportsball>::value, equal_to(true));
   });
+  _.test("has a getScore method", []() {
+    expect(
+        std::is_member_function_pointer<decltype(&Sportsball::getScore)>::value,
+        equal_to(true));
+  });
  });
