c45bc68f551e1648682c1b76d8f0dfcbadcef805
Charles Ellis <hamled@hamled.name>
2019-11-02 12:04:11 -0700

    Sportsball has an addEntry method

--- test/sportsball.test.cpp
+++ test/sportsball.test.cpp
@@ -10,6 +10,9 @@ suite<> sportsball("Sportsball", [](auto &_) {
   _.test("has a getScore method", []() {
     expect(std::is_member_function_pointer<decltype(&Sportsball::getScore)>::value, equal_to(true));
   });
+  _.test("has an addEntry method", []() {
+    expect(std::is_member_function_pointer<decltype(&Sportsball::addEntry)>::value, equal_to(true));
+  });
   _.subsuite("getScore", [](auto &_) {
     _.test("scores 0 points for no runners", []() {
       Sportsball sb;
