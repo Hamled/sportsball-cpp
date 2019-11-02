64ff586231a46347504870cbe668f29252fb9d7a
Charles Ellis <hamled@hamled.name>
2019-11-02 12:06:03 -0700

    getScore scores points for 0+ runners before home run

--- test/sportsball.test.cpp
+++ test/sportsball.test.cpp
@@ -19,5 +19,11 @@ suite<> sportsball("Sportsball", [](auto &_) {
 
       expect(sb.getScore(), equal_to("Home: 0 Away: 0"));
     });
+    _.test("scores points for 0+ runners before home run", []() {
+      Sportsball sb;
+      for(auto e : {3,2,1,4,3,2,1}) sb.addEntry(e);
+
+      expect(sb.getScore(), equal_to("Home: 0 Away: 4"));
+    });
   });
 });
