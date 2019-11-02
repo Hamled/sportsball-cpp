7b2c76467d8504ee03142f6345842667e797a42a
Charles Ellis <hamled@hamled.name>
2019-11-02 12:18:36 -0700

    tests pass

--- src/sportsball.cpp
+++ src/sportsball.cpp
@@ -3,11 +3,38 @@
 
 #include "sportsball.hpp"
 
+Sportsball::Sportsball() {
+  resetFrame();
+}
+
 std::string Sportsball::getScore() const {
-  return (boost::format("Home: 0 Away: %1%") % (runners - unscored)).str();
+  const int cur_away = is_away ? away + score() : away;
+  const int cur_home = is_away ? home : home + score();
+  return (boost::format("Home: %1% Away: %2%") % cur_home % cur_away).str();
 }
 
 void Sportsball::addEntry(int entry) {
+  if(!entry) {
+    if(++outs > 2) {
+      if(is_away)
+        away += score();
+      else
+        home += score();
+
+      resetFrame();
+    }
+    return;
+  }
+
   runners++;
   unscored = std::min(BASES - entry, unscored) + 1;
+}
+
+int Sportsball::score() const {
+  return runners - unscored;
+}
+
+void Sportsball::resetFrame() {
+  runners = unscored = outs = 0;
+  is_away = !is_away;
 }
--- src/sportsball.hpp
+++ src/sportsball.hpp
@@ -5,8 +5,16 @@ private:
   static const int BASES = 3;
   int runners = 0;
   int unscored = 0;
+  int outs = 0;
+  int away = 0;
+  int home = 0;
+  int is_away = false;
+
+  int score() const;
+  void resetFrame();
 
 public:
+  Sportsball();
   std::string getScore() const;
   void addEntry(int entry);
 };
